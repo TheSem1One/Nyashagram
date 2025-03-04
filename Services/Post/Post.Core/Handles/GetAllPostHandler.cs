d8652aeac827c4b3ae8c6e3a982430aba86df930
TheSem1One <animehub021@gmail.com>
2025-03-04 17:54:29 +0200

    Adding PostMapper and PostMapperProfile

---
+++ Services/Post/Post.Core/Handles/GetAllPostHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Post.Application.Queries;
+using Post.Application.Responses;
+using Post.Domain.Reposetories;
+
+namespace Post.Application.Handles
+{
+    public class GetAllPostHandler : IRequestHandler<GetAllPostQuerry, IList<PostResponse>>
+    {
+        private readonly IPostReposetory _postReposetory;
+        public GetAllPostHandler(IPostReposetory postReposetory)
+        {
+            _postReposetory = postReposetory;
+        }
+        public Task<IList<PostResponse>> Handle(GetAllPostQuerry request, CancellationToken cancellationToken)
+        {
+            var postList = await _postReposetory.GetAllPost();
+            var postResponseList 
+        }
+    }
+}
--- Services/Post/Post.Core/Mappers/PostMappingProfile.cs
+++ Services/Post/Post.Core/Mappers/PostMappingProfile.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Post.Application.Responses;
 
 namespace Post.Application.Mappers
 {
-    public class PostMappingProfile
+    public class PostMappingProfile : Profile
     {
+        public PostMappingProfile()
+        {
+            CreateMap<Domain.Entities.Post, PostResponse>().ReverseMap();
+        }
     }
 }
