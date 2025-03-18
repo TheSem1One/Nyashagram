8bb6a26837ca510f32ecc22e2488467a2b8be81b
TheSem1One <animehub021@gmail.com>
2025-03-18 12:24:00 +0200

    adding Folders and FileManagerResponse.cs

---
+++ Services/FileManager/FileManager.Application/Responses/FileManagerResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Application.Responses
+{
+    public class FileManagerResponse
+    {
+        public List<string> FileUrl { get; set; } = null!;
+    }
+}
---
+++ Services/FileManager/FileManager.Domain/Reposetories/IFileManagerReposetory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace FileManager.Domain.Reposetories
+{
+    public interface IFileManagerReposetory
+    {
+        Task<IEnumerable<string>> CreateImage(List<IFormFile> files);
+        Task<bool> DeleteImage(string imageUrl);
+    }
+}
