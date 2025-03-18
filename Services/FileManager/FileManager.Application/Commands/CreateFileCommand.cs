210157ad854c682c659185ac1dfb26f96f1043e8
TheSem1One <animehub021@gmail.com>
2025-03-18 12:28:07 +0200

    Adding FIle Command.cs

---
+++ Services/FileManager/FileManager.Application/Commands/CreateFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Application.Responses;
+
+namespace FileManager.Application.Commands
+{
+    public class CreateFileCommand : IRequest<FileManagerResponse>
+    {
+        public List<string> FileUrl { get; set; } = null!;
+
+        public CreateFileCommand(List<string> fileUrl)
+        {
+            FileUrl = fileUrl;
+        }
+    }
+}
