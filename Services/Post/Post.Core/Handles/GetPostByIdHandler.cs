027537455180cab1423e774f8ab9308f960dee17
TheSem1One <animehub021@gmail.com>
2025-03-24 10:59:00 +0200

    Adding Get PostById Handler

---
+++ Services/Post/Post.Core/Handles/GetPostByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Post.Application.Mappers;
+using Post.Application.Queries;
+using Post.Application.Responses;
+using Post.Domain.Reposetories;
+
+namespace Post.Application.Handles
+{
+    public class GetPostByIdHandler : IRequestHandler<GetPostByIdQuerry, GetPostByIdResponse>
+    {
+
+        private readonly IPostReposetory _postReposetory;
+        public GetPostByIdHandler(IPostReposetory postReposetory, IMapper mapper)
+        {
+            _postReposetory = postReposetory;
+        }
+        public async Task<GetPostByIdResponse> Handle(GetPostByIdQuerry request, CancellationToken cancellationToken)
+        {
+            var post = await _postReposetory.GetPostById(request.PostId);
+            var postResponse = PostMapper.Mapper.Map<Domain.Entities.Post, GetPostByIdResponse>(post);
+            return postResponse;
+        }
+    }
+}
