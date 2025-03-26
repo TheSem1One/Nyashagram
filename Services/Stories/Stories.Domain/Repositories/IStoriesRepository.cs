a3f737c1e34895a9172e615a9ba25834091a8ee6
TheSem1One <animehub021@gmail.com>
2025-03-26 15:47:36 +0200

    Adding Stories Interface and Implement it

--- Services/Stories/Stories.Domain/Repositories/IStoriesRepository.cs
+++ Services/Stories/Stories.Domain/Repositories/IStoriesRepository.cs
@@ -10,10 +10,10 @@ namespace Stories.Domain.Repositories
 {
     public interface IStoriesRepository
     {
-        Task<IEnumerable<Short>> GetPost();
+        Task<IEnumerable<Short>> GetStories();
         Task<IEnumerable<Short>> GetStoriesNickName(string nickName);
         Task<Short> GetStoriesById(string id);
-        Task<string> CreatePost(string Dto);
-        Task<bool> DeletePost(string id);
+        Task<string> CreateStories(string Dto);
+        Task<bool> DeleteStories(string id);
     }
 }
---
+++ Services/Stories/Stories.Infrastructure/Repositories/StoriesRepositories.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stories.Domain.Entity;
+using Stories.Domain.Repositories;
+
+namespace Stories.Infrastructure.Repositories
+{
+    public class StoriesRepositories : IStoriesRepository
+    {
+        public Task<string> CreateStories(string Dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteStories(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Short>> GetStories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Short> GetStoriesById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Short>> GetStoriesNickName(string nickName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
