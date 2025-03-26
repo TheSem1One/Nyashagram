47efa72bfb5a227d9015f4878c4e8350ff91c101
TheSem1One <animehub021@gmail.com>
2025-03-26 18:30:59 +0200

    added functionality to the StoriesRepository

--- Services/Stories/Stories.Domain/Repositories/IStoriesRepository.cs
+++ Services/Stories/Stories.Domain/Repositories/IStoriesRepository.cs
@@ -5,15 +5,15 @@ using System.Runtime.InteropServices.JavaScript;
 using System.Text;
 using System.Threading.Tasks;
 using Stories.Domain.Entity;
+using Stories.Domain.Entity.DTO;
 
 namespace Stories.Domain.Repositories
 {
     public interface IStoriesRepository
     {
-        Task<IEnumerable<Short>> GetStories();
         Task<IEnumerable<Short>> GetStoriesNickName(string nickName);
         Task<Short> GetStoriesById(string id);
-        Task<string> CreateStories(string Dto);
+        Task<string> CreateStories(ShortsDTO shortsDto);
         Task<bool> DeleteStories(string id);
     }
 }
--- Services/Stories/Stories.Infrastructure/Repositories/StoriesRepositories.cs
+++ Services/Stories/Stories.Infrastructure/Repositories/StoriesRepositories.cs
@@ -1,38 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Driver;
 using Stories.Domain.Entity;
+using Stories.Domain.Entity.DTO;
 using Stories.Domain.Repositories;
+using Stories.Infrastructure.Presistence;
 
 namespace Stories.Infrastructure.Repositories
 {
     public class StoriesRepositories : IStoriesRepository
     {
-        public Task<string> CreateStories(string Dto)
+        private readonly IStoriesContext _context;
+        public StoriesRepositories(IStoriesContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        async Task<string> IStoriesRepository.CreateStories(ShortsDTO Dto)
+        {
+            var stories = new Short
+            {
+                CreatorNickName = Dto.CreatorNickName,
+                StoriesImageUrl = Dto.StoriesImageUrl,
+                ExpireTime = DateTime.Now.AddDays(1)
+            };
+            await _context.Shorts.InsertOneAsync(stories);
+            return stories.StoriesId;
         }
 
-        public Task<bool> DeleteStories(string id)
+        async Task<bool> IStoriesRepository.DeleteStories(string id)
         {
-            throw new NotImplementedException();
+            var deleteShorts = await _context.Shorts.DeleteOneAsync(s => s.StoriesId == id);
+            return deleteShorts.IsAcknowledged & deleteShorts.DeletedCount > 0;
         }
 
-        public Task<IEnumerable<Short>> GetStories()
+
+        async Task<Short> IStoriesRepository.GetStoriesById(string id)
         {
-            throw new NotImplementedException();
+            return await _context
+                .Shorts
+                .Find(stories => stories.StoriesId == id)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<Short> GetStoriesById(string id)
+        async Task<IEnumerable<Short>> IStoriesRepository.GetStoriesNickName(string nickName)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Short>> GetStoriesNickName(string nickName)
-        {
-            throw new NotImplementedException();
+            return await _context.Shorts
+                .Find(s => s.CreatorNickName.ToLower() == nickName.ToLower())
+                .ToListAsync();
         }
     }
 }
