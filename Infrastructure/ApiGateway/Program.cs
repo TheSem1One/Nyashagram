ff3d859ec992da961866bf5ac91f4f05ba5583ae
TheSem1One <animehub021@gmail.com>
2025-04-29 20:01:32 +0300

    deleting swagger

--- Infrastructure/ApiGateway/Program.cs
+++ Infrastructure/ApiGateway/Program.cs
@@ -1,39 +1,28 @@
-using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Nyashagram Gateway API",
-        Version = "v1",
-    });
-});
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
         builder =>
         {
             builder.AllowAnyOrigin()
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
+                .AllowAnyMethod()
+                .AllowAnyHeader();
         });
 });
+
 builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
+
 app.UseCors("AllowAll");
-app.UseSwaggerForOcelotUI(options =>
-{
-    options.PathToSwaggerGenerator = "/swagger/docs";
-});
+
 await app.UseOcelot();
 
 app.Run();
