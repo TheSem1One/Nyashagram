b1d6a598edc828f8352cc32dc49f72763af25520
TheSem1One <animehub021@gmail.com>
2025-04-29 15:25:40 +0300

    fix

--- Infrastructure/ApiGateway/Program.cs
+++ Infrastructure/ApiGateway/Program.cs
@@ -6,7 +6,19 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
+app.UseCors("AllowAll");
 await app.UseOcelot();
 
 app.Run();
